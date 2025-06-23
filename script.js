a7fb62abb7d6cc3ebfd56e884a02ac14bf11a3e0
Misbah1992 <misbah199219@gmail.com>
2025-06-23 19:25:03 +0500

    working

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 
-gsap.to('#page2 h1',{
-   transform:"translateX(-195%)",
-   scrollTrigger:{
-    trigger:"#page2",
-    scroller:"body",
-    markers:true,
-    start:"top 0%",
-    end:"top -150%",
-    scrub:2,
-    pin:true
-   }
+var tl =gsap.timeline()
+
+
+tl.to("#full",{
+   right:0,
+   duration:0.8
+})
+
+tl.from("#full h4",{
+   x:150
 })
